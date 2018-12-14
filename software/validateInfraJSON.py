46b74678327b000dcc03d07debb6dd953d843e01
Kimmy <kavisha.btechit@gmail.com>
2018-12-14 18:42:54 +0100

    fixed schema

--- software/validateInfraJSON.py
+++ software/validateInfraJSON.py
@@ -10,7 +10,8 @@ Created on Tue Sep 11 11:39:04 2018
 
 import json
 import jsonschema
-from jsonschema import Draft7Validator
+#from jsonschema import Draft7Validator
+import jsonschema
 #import os
 
 filename =  r'/Users/kavisha/Desktop/githubpvt/InfraJSON/sampledata/example_infrajson_corefeatures.json'
@@ -30,20 +31,20 @@ js = json.loads(schema)
 #print ('file://%s/' % os.path.abspath(os.path.dirname(__file__)) +"cityjson/schema/v07/")
 resolver = jsonschema.RefResolver('file:///Users/kavisha/Desktop/githubpvt/InfraJSON/schema/v01/', None)
 #print (resolver)
-try:
-#    jsonschema.validate(j,js,resolver=resolver)
-    Draft7Validator.check_schema(js)
-    print("Passed derived schema.")
-#except Exception as ex:
-#    print("Failed derived schema: %s" % ex)
-except jsonschema.ValidationError as e:
-    print ("hi",e.message)
-except jsonschema.SchemaError as e:
-    print ("ho",e)
+#try:
+##    jsonschema.validate(j,js,resolver=resolver)
+#    Draft7Validator.check_schema(js)
+#    print("Passed derived schema.")
+##except Exception as ex:
+##    print("Failed derived schema: %s" % ex)
+#except jsonschema.ValidationError as e:
+#    print ("hi",e.message)
+#except jsonschema.SchemaError as e:
+#    print ("ho",e)
 
 try:
-#    jsonschema.validate(j,js,resolver=resolver)
-    Draft7Validator(js,resolver=resolver).validate(j)
+    jsonschema.validate(j,js,resolver=resolver)
+#    Draft7Validator(js,resolver=resolver).validate(j)
     print("Passed derived schema.")
 #except Exception as ex:
 #    print("Failed derived schema: %s" % ex)
